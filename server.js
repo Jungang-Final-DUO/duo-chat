90230b47babf1ae198b0363d3cc432d38b8ebf58
jhlee <jhlee9462@github.com>
2023-06-19 22:46:11 +0900

    complete - bug fix

--- server.js
+++ server.js
@@ -1,33 +1,40 @@
-const http = require('http');
-const express = require('express');
-const socketio = require('socket.io');
-const formatMessage = require('./utils/messages');
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+(() => {
+    const http = require('http');
+    const express = require('express');
+    const socketio = require('socket.io');
+    const formatMessage = require('./utils/messages');
 
-const app = express();
-const server = http.createServer(app);
+    const app = express();
+    const server = http.createServer(app);
 
-const io = socketio(server, {
-    cors: {
-        origin: "http://localhost:8282",
-        methods: ["GET", "POST"],
-    }
-});
+    const io = socketio(server, {
+        cors: {
+            origin: "http://localhost:8282",
+            methods: ["GET", "POST"],
+        }
+    });
 
-const adminName = 'WOULD U DUO'
+    const adminName = 'WOULD U DUO'
 
 //Run when client connect
-io.on('connection', socket => {
+    io.on('connection', socket => {
+        console.log('연결됨');
+        //listen for chatMessage
+        socket.on('chatMessage', (message) => {
+            const {username, room, msg} = message;
+            console.log(username, msg, room);
+            io.emit('message', formatMessage(username, room, msg));
+        })
 
-    //listen for chatMessage
-    socket.on('chatMessage', (message) => {
-        const {username, room, msg} = message;
-        console.log(username, msg, room);
-        io.emit('message', formatMessage(username, room, msg));
-    })
-
-});
+    });
 
 
-const PORT = 3000 || process.env.PORT;
+    const PORT = 3000 || process.env.PORT;
 
-server.listen(PORT, () => console.log(`server running on port ${PORT}`));
+    server.listen(PORT, () => console.log(`server running on port ${PORT}`));
+
+})();
